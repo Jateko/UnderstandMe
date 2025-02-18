a516c42ae32e2c3e047b2c6c5e9bdb5762859ade
Pavel Afonin <pafonin@tango.me>
2025-02-18 13:31:58 +0100

    add GTM

---
+++ UnderstardMe/UnderstardMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstardMeApp.swift
+//  UnderstardMe
+//
+//  Created by Pavel Afonin on 18.02.25.
+//
+
+import SwiftUI
+
+@main
+struct UnderstardMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
